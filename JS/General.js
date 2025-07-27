0887704d15188621baed1c371282e3d4cc532020
Gianluca Morello <107130109+Gianlu1107@users.noreply.github.com>
2025-07-27 15:44:51 +0200

    upload a backup

--- JS/General.js
+++ JS/General.js
@@ -19,16 +19,3 @@ const navinicial = document.querySelector('.nav-logo');
 navinicial.addEventListener('click', () => {
     window.location.href = "../HTML/Index.html";
 });
-
-// =====================
-//   NASCONDI SPINNER AL CARICAMENTO
-// =====================
-
-window.addEventListener('load', () => {
-    const loader = document.getElementById('loading');
-    if (loader) {
-        loader.style.transition = 'opacity 0.5s ease';
-        loader.style.opacity = '0';
-        setTimeout(() => loader.style.display = 'none', 500);
-    }
-});
